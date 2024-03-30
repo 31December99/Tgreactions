8f9bd12bc4b3f1fda86970c2edaf83b70c175c0a
midnight <jw79985@gmail.com>
2024-03-30 17:48:41 +0100

    Added input user validation function

--- reactions.py
+++ reactions.py
@@ -3,24 +3,27 @@ import asyncio
 from group import Group
 
 
+def validate_user_input(topics: list) -> int:
+    while True:
+        index = input(f"Choose which topic you want to check (0-{len(topics) - 1}): ")
+        if index.isnumeric():
+            topic_index = int(index)
+            if 0 <= topic_index <= len(topics) - 1:
+                topic_id, topic_title = topics[topic_index]
+                return topic_id
+
+
 async def main():
     # New Group istance
     group = Group()
     # Connect to group
     await group.connect()
     # Get your Topics list
-    topic_id = None
+
     list_of_topics = await group.forum_topics()
     if list_of_topics:
-        while True:
-            index = input(f"Choose which topic you want to check (0-{len(list_of_topics) - 1}): ")
-            if index.isnumeric():
-                topic_index = int(index)
-                if 0 <= topic_index <= len(list_of_topics) - 1:
-                    topic_id, topic_title = list_of_topics[topic_index]
-                    break
-
-    await group.topic(topic_id)
+        topic_id = validate_user_input(list_of_topics)
+        await group.topic(topic_id)
     loop.stop()
 
 
